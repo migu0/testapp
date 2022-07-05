e02c5f22d4a1e1864415f876739393f9584b2d80
Michael Imstepf <michael.imstepf@gmail.com>
2022-07-05 19:38:27 +1000

    Initial commit

---
+++ app/assets/javascripts/administrate/application.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require selectize
+//= require moment
+//= require datetime_picker
+//= require trix
+//= require actiontext
+//= require_tree .
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  namespace :admin do
+      resources :posts
+
+      root to: "posts#index"
+    end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
